28aace56014f062befcea189b20e1e27d059c03c
Alex Jackson <contact@ajaxbits.com>
2023-08-16 09:52:16 -0500

    chore: clean up flake

--- flake.nix
+++ flake.nix
@@ -23,57 +23,60 @@
       }: let
         lib = pkgs.lib;
         stagingUrl = "http://127.0.0.1:8080";
-      in {
-        packages.blog = pkgs.stdenv.mkDerivation {
-          pname = "ajaxbits";
-          version = "0.0.0";
-          src = ./.;
-          nativeBuildInputs = [
-            pkgs.zola
-            pkgs.nodePackages.npm
-            pkgs.nodePackages.uglify-js
-            pkgs.openssl
-          ];
-          buildPhase = ''
-            set -euo pipefail
-            export HOME=$(pwd)
-            zola build
-            npm run abridge
-            zola build
-          '';
-          installPhase = "cp -r public $out";
 
-          meta = with lib; {
-            description = "My website";
-            homepage = "https://blog.ajaxbits.com";
-            changelog = "https://github.com/ajaxbits/blog/commits/main";
-            licence = licences.agpl3Only;
-            maintainers = [
-              {
-                name = "Alex Jackson";
-                email = "contact@ajaxbits.com";
-                github = "ajaxbits";
-                githubId = 45179933;
-                keys = [
-                  {fingerprint = "ED00BE54AAF8A59F4E409560798DF4ACC8A798B3";}
-                ];
-              }
+        blogBuilder = {staging ? false}:
+          pkgs.stdenv.mkDerivation {
+            pname = "ajaxbits";
+            version =
+              if staging
+              then "staging"
+              else "0.0.0";
+            src = ./.;
+            nativeBuildInputs = [
+              pkgs.zola
+              pkgs.nodePackages.npm
+              pkgs.nodePackages.uglify-js
+              pkgs.openssl
             ];
+            buildPhase = ''
+              set -euo pipefail
+              export HOME=$(pwd)
+
+              zola build \
+                ${(lib.optionalString staging "--drafts --base-url=${stagingUrl}")}
+
+              npm run abridge
+
+              zola build \
+                ${(lib.optionalString staging "--drafts --base-url=${stagingUrl}")}
+            '';
+            installPhase = "cp -r public $out";
+
+            meta = with lib; {
+              description = "My website";
+              homepage = "https://blog.ajaxbits.com";
+              changelog = "https://github.com/ajaxbits/blog/commits/main";
+              licence = licences.agpl3Only;
+              maintainers = [
+                {
+                  name = "Alex Jackson";
+                  email = "contact@ajaxbits.com";
+                  github = "ajaxbits";
+                  githubId = 45179933;
+                  keys = [
+                    {fingerprint = "ED00BE54AAF8A59F4E409560798DF4ACC8A798B3";}
+                  ];
+                }
+              ];
+            };
           };
-        };
+      in {
+        packages.default = config.packages.prod;
+        apps.default = config.apps.prod;
 
-        packages.default = config.packages.blog;
+        packages.prod = blogBuilder {};
 
-        packages.staging = config.packages.blog.overrideAttrs (old: {
-          version = "staging";
-          buildPhase = ''
-            set -euo pipefail
-            export HOME=$(pwd)
-            zola build --drafts --base-url=${stagingUrl}
-            npm run abridge
-            zola build --drafts --base-url=${stagingUrl}
-          '';
-        });
+        packages.staging = blogBuilder {staging = true;};
 
         apps.staging = {
           type = "app";
@@ -82,16 +85,21 @@
               --directory ${config.packages.staging}
           '';
         };
-        
-        apps.default = config.apps.staging;
+
+        apps.prod = {
+          type = "app";
+          program = pkgs.writeShellScriptBin "server" ''
+            ${pkgs.python3Minimal}/bin/python3 -m http.server 8080 \
+              --directory ${config.packages.prod}
+          '';
+        };
 
         devShells.default = pkgs.mkShell {
-          packages = with pkgs; [
-            zola
-            alejandra.defaultPackage.${system}
-            nodePackages.npm
-            nodePackages.uglify-js
-          ];
+          packages =
+            config.packages.prod.nativeBuildInputs
+            ++ [
+              alejandra.defaultPackage.${system}
+            ];
         };
       };
     };
